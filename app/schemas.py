d0cb7f14b7b25bc7ac84abee8302906e42db9e4e
wuiquique <luisenriquem15@gmail.com>
2025-03-17 10:46:39 -0600

    new: api schemas

---
+++ app/schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+class UserLogin(BaseModel):
+    username: str
+
+class UserCreate(BaseModel):
+    name: str
+    username: str
+
+class ProjectCreate(BaseModel):
+    name: str
+    userId: int
+
+class TaskCreate(BaseModel):
+    title: str
+    isCompleted: bool
+    due: str
+    projectId: int
